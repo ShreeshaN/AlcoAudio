dcae1a61ebe5d87a6d33298fa6657e056fe8a2a3
ShreeshaN <nshreesha2011@gmail.com>
2020-02-19 08:12:01 -0500

    added readme, updated timestamps

---
+++ README.md
@@ -0,0 +1,1 @@
+### Detection of Alcohol induced intoxication through voice using Neural Networks
--- alcoaudio/__init__.py
+++ alcoaudio/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-@created on: 2/17/20,
+@created on: 02/16/20,
 @author: Shreesha N,
 @version: v0.0.1
 @system name: badgod
