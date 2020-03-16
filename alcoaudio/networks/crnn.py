18dd6e46471f6570e0024525899912c798f3ae9b
ShreeshaN <nshreesha2011@gmail.com>
2020-03-16 11:27:01 -0400

    crnn model test

--- alcoaudio/networks/crnn.py
+++ alcoaudio/networks/crnn.py
@@ -18,8 +18,7 @@ import cv2
 import numpy as np
 
 
-class ConvLSTM(nn.Module):
-
+class ConvModule(nn.Module):
     def __init__(self):
         """
         You can add additional arguments as you need.
@@ -27,38 +26,28 @@ class ConvLSTM(nn.Module):
         member variables.
         """
 
-        super(ConvLSTM, self).__init__()
+        super(ConvModule, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1)
+        self.conv1_bn = nn.BatchNorm2d(64)
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=4, stride=1)
+        self.conv2_bn = nn.BatchNorm2d(128)
+        self.pool1 = nn.MaxPool2d(kernel_size=4, stride=2)
         self.dropout0 = nn.Dropout(p=0.4)
 
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=[1, 2])
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1)
+        self.conv3_bn = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=[1, 2])
+        self.conv4_bn = nn.BatchNorm2d(128)
         self.pool2 = nn.MaxPool2d(kernel_size=4, stride=2)
 
         self.conv5 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=[1, 2])
+        self.conv5_bn = nn.BatchNorm2d(64)
         self.pool3 = nn.MaxPool2d(kernel_size=3, stride=[1, 2])
 
         self.depth_conv = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1)
 
-        self.lstm = nn.LSTM(9, 128, 1, bias=True)
-
-        self.fc1 = nn.Linear(128, 128)
-        self.dropout1 = nn.Dropout(p=0.3)
-        self.fc2 = nn.Linear(128, 32)
-        self.fc3 = nn.Linear(32, 1)
-
     def forward(self, x):
-        """
-        In the forward function we accept a Tensor of input data and we must return
-        a Tensor of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Tensors.
-        """
-        # x = self.reshape_for_pytorch(x)
-        x = tensor(x).unsqueeze(1)
-
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = self.pool1(x)
@@ -72,22 +61,42 @@ class ConvLSTM(nn.Module):
         x = self.pool3(x)
 
         x = self.depth_conv(x)
+        return x
+
+
+class RNNModule(nn.Module):
+    def __init__(self):
+        super(RNNModule, self).__init__()
+        self.lstm = nn.LSTM(1, 128, 1, bias=True)
+        self.fc1 = nn.Linear(1152, 1)
+
+    def forward(self, x):
+        rnn_out, _ = self.lstm(x)
+        rnn_out = rnn_out.permute(1, 2, 0)
+        x = rnn_out.contiguous().view(-1, rnn_out.shape[1] * rnn_out.shape[2])
+        x = self.fc1(x)
+        return x
+
+
+class CRNN(nn.Module):
+    def __init__(self):
+        super(CRNN, self).__init__()
+        self.cnn = ConvModule()
+        self.rnn = RNNModule()
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        x = tensor(x).unsqueeze(1)
+        x = self.cnn(x)
 
         # LSTM from here
         x = x.squeeze(1).permute(2, 0, 1)  # Converting from (B,C,H,W)->(B,H,W)->(W,B,H)
-
-        lstm_out, _ = self.lstm(x)
-        # lstm_out = lstm_out.permute(1, 0, 2)  # Converting it back to (B,W,H) from (W,B,H)
-
-
-        # Flattening to feed it to FFN
-        x = lstm_out[-1].view(-1, lstm_out[-1].shape[1])
-        x = F.relu(self.fc1(x))
-        x = self.dropout1(x)
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
+        output = self.rnn(x)
+        return output
 
 # import torch
 # import torch.nn as nn
