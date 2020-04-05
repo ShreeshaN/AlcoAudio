5c938d74dfe4748c9d13c26eedb75ece27bd8af5
ShreeshaN <nshreesha2011@gmail.com>
2020-04-04 21:44:06 -0400

    testing bidirectional lstm

--- alcoaudio/networks/crnn.py
+++ alcoaudio/networks/crnn.py
@@ -28,54 +28,43 @@ class ConvModule(nn.Module):
 
         super(ConvModule, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, stride=[1, 2])
         self.conv1_bn = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=[1, 2])
         self.conv2_bn = nn.BatchNorm2d(128)
         self.pool1 = nn.MaxPool2d(kernel_size=4, stride=2)
         self.dropout0 = nn.Dropout(p=0.4)
 
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=[1, 2])
         self.conv3_bn = nn.BatchNorm2d(256)
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=[1, 2])
-        self.conv4_bn = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=64, kernel_size=3, stride=[1, 2])
+        self.conv4_bn = nn.BatchNorm2d(64)
         self.pool2 = nn.MaxPool2d(kernel_size=4, stride=2)
-
-        self.conv5 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=[1, 2])
-        self.conv5_bn = nn.BatchNorm2d(64)
-        self.pool3 = nn.MaxPool2d(kernel_size=3, stride=[1, 2])
-
-        self.depth_conv = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1)
+        #
+        # self.conv5 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=[1, 2])
+        # self.conv5_bn = nn.BatchNorm2d(64)
+        # self.pool3 = nn.MaxPool2d(kernel_size=3, stride=[1, 2])
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
         x = self.pool1(x)
         x = self.dropout0(x)
 
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = self.pool2(x)
-
-        x = F.relu(self.conv5(x))
-        x = self.pool3(x)
-
-        x = self.depth_conv(x)
+        x = F.relu(self.conv3_bn(self.conv3(x)))
+        x = F.relu(self.conv4_bn(self.conv4(x)))
         return x
 
 
 class RNNModule(nn.Module):
     def __init__(self):
         super(RNNModule, self).__init__()
-        self.lstm = nn.LSTM(1, 128, 2, bias=True, dropout=0.2, bidirectional=True)
+        self.lstm = nn.LSTM(704, 128, 2, bias=True, dropout=0.2, bidirectional=True)
         self.fc1 = nn.Linear(5120, 1)
 
     def forward(self, x):
         rnn_out, _ = self.lstm(x)
-
-        # (seq_len, batch, num_directions * hidden_size) -> (batch, num_directions * hidden_size, seq_len)
         rnn_out = rnn_out.permute(1, 2, 0)
-
         x = rnn_out.contiguous().view(-1, rnn_out.shape[1] * rnn_out.shape[2])
         x = self.fc1(x)
         return x
@@ -97,6 +86,9 @@ class CRNN(nn.Module):
         x = self.cnn(x)
 
         # LSTM from here
-        x = x.squeeze(1).permute(2, 0, 1)  # Converting from (B,C,H,W)->(B,H,W)->(W,B,H)
+        batch_size = x.shape[0]
+        x = x.view(batch_size, x.shape[1] * x.shape[2], x.shape[3])
+        x = x.permute(2, 0, 1)  # Converting from (B,H,W)->(W,B,H)
+
         output = self.rnn(x)
         return output
