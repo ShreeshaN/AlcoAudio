c39ac3464acbffc15ae0c56df067293d93233684
ShreeshaN <nshreesha2011@gmail.com>
2020-05-11 12:20:51 -0400

    increased dropout

--- alcoaudio/networks/convnet.py
+++ alcoaudio/networks/convnet.py
@@ -31,7 +31,7 @@ class ConvNet(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
         self.conv2_bn = nn.BatchNorm2d(64)
         self.pool1 = nn.MaxPool2d(kernel_size=4, stride=2)
-        self.dropout0 = nn.Dropout(p=0.3)
+        self.dropout0 = nn.Dropout(p=0.4)
 
         self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=[1, 2])
         self.conv4_bn = nn.BatchNorm2d(64)
