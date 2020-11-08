14e219bc9bd943fae76dfaace18c9c0b3d6d661e
ShreeshaN <nshreesha2011@gmail.com>
2020-11-07 21:47:31 -0500

    finetuning resnet-1

--- alcoaudio/runners/resnet_pretrain.py
+++ alcoaudio/runners/resnet_pretrain.py
@@ -257,7 +257,7 @@ class ResNetRunner:
 
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
-                    binary_preds = torch.where(outputs > to_tensor(0.5), to_tensor(1), to_tensor(0))
+                    binary_preds = torch.where(outputs > to_tensor(self.threshold), to_tensor(1), to_tensor(0))
                     total_preds.extend(binary_preds)
 
                     running_corrects += torch.sum(binary_preds == labels)
@@ -272,7 +272,7 @@ class ResNetRunner:
                         phase, epoch_loss, epoch_acc, epoch_uar))
 
                 # deep copy the model
-                if phase == 'val' and epoch_acc > best_acc:
+                if phase == 'dev' and epoch_acc > best_acc:
                     best_acc = epoch_acc
                     best_model_wts = copy.deepcopy(model.state_dict())
 
@@ -312,15 +312,15 @@ class ResNetRunner:
         num_ftrs = model_ft.fc.in_features
         # # Here the size of each output sample is set to 2.
         # # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-        model_ft.fc = nn.Linear(num_ftrs, 1)
+        model_ft.fc = nn.Linear(num_ftrs, self.num_classes)
         model_ft = model_ft.to(self.device)
         criterion = nn.BCEWithLogitsLoss(pos_weight=to_tensor(self.pos_weight, device=self.device))
 
         # Observe that all parameters are being optimized
-        optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.0001, momentum=0.9)
+        optimizer_ft = optim.SGD(model_ft.parameters(), lr=self.learning_rate, momentum=0.9)
 
         # Decay LR by a factor of 0.1 every 7 epochs
         exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
         self.train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                         num_epochs=25)
+                         num_epochs=self.epochs)
